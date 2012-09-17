a42f64b7ba4e5f428551f614a0f05cb56abfb1ec
smalls <small@cybertronic.com>
2012-09-17 09:21:47 -0700

    change this to a lein-aware project to use the plugin

---
+++ README.md
@@ -0,0 +1,9 @@
+# splendous.net
+
+Blog entries appearing on splendous.net, generated using..
+
+## License
+
+Copyright © 2012 Matt Small
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject splendous.net "0.1.0-SNAPSHOT"
+            :description "blog entries for splendous.net"
+            :url "http://splendous.net/"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[org.clojure/clojure "1.4.0"]]
+            :plugins [[smallblog-static "1.0.0-SNAPSHOT"]])
