bc811dea0300d335703c7fa31faa995adc59b16b
smalls <small@cybertronic.com>
2012-10-20 09:50:26 -0700

    try a local repo for smallblog-static depdencies (this should be temporary until it's "done" and goes up somewhere public)

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.4.0"]]
-            :plugins [[smallblog-static "1.0.0-SNAPSHOT"]])
+            :plugins [[smallblog-static "1.0.0-SNAPSHOT"]]
+            :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))})
