4a7c09e6be573c89c55ca8137f38482d36652465
smalls <small@cybertronic.com>
2012-10-22 08:35:52 -0700

    serve index.html from /

--- src/net/splendous/routes.clj
+++ src/net/splendous/routes.clj
@@ -7,5 +7,13 @@
            (route/resources "/" {:root "static"})
            (route/not-found "Page not found"))
 
+(comment from http://stackoverflow.com/questions/7729628/serve-index-html-at-by-default-in-compojure)
+(defn wrap-dir-index [handler]
+    (fn [req]
+        (handler
+            (update-in req [:uri]
+                       #(if (= "/" %) "/index.html" %)))))
+
 (def app
-    (-> (handler/site main-routes)))
+    (-> (handler/site main-routes)
+        (wrap-dir-index)))
