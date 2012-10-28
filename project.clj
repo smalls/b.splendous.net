d92c859de2c8cb2dd1d681fbf64f5d9690aab2eb
smalls <small@cybertronic.com>
2012-10-28 01:50:19 -0700

    update project.clj to require compojure

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
             :url "http://splendous.net/"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.4.0"]]
-            :plugins [[smallblog-static "1.0.0-SNAPSHOT"]]
-            :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))})
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [compojure "1.1.3"]]
+            :resource-paths ["target/web"]
+            :plugins [[smallblog-static "1.0.0-SNAPSHOT"]
+                      [lein-ring "0.7.1"]]
+            :repositories {"local" {:url ~(str (.toURI (java.io.File. "maven_repository")))
+                                    :update :always}}
+            :ring {:handler net.splendous.routes/app})
